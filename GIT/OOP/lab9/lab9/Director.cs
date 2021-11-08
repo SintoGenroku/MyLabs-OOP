3c447859ea7725e4da071c9dc3e17734092dc699
SintoGenroku <borozda.a.s@gmail.com>
2021-11-08 19:34:55 +0300

    delegates, events

---
+++ GIT/OOP/lab9/lab9/Director.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab9
+{
+    public delegate void IsJobDone();
+    
+    class Director
+    {
+        IsJobDone solution;
+       // event EndOfMonth SalaryTime; 
+        private int salary;
+        private string post;
+        private int lvl;
+        private bool jobDone;
+
+        public Director(string post, int salary, int lvl = 1, bool jobDone = false) 
+        {
+            this.salary = salary;
+            this.post = post;
+            this.lvl = lvl;
+            this.jobDone = jobDone;
+        }
+
+        public void EndOfMonth()
+        {
+            if (jobDone)
+            {
+                solution = ToRaise;
+            }
+            else
+            {
+                solution = Penalty;
+            }
+        }
+        public void ToRaise() =>        
+            ++lvl;
+
+        public void ToReduce() =>
+            lvl = (lvl > 1) ? --lvl : lvl;
+
+        public void Bonus() =>
+            salary += 500;
+        public void Penalty() =>
+            salary = (salary>500) ? salary -= 500 : salary = 0;
+        
+
+    }
+}
---
+++ GIT/OOP/lab9/lab9/StandartDelegates.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab9
+{
+
+    public delegate string Action(string str);
+
+    public static class StrOperations
+    {
+        static Action StrChanger = new Action(VowelDel);
+        
+        public static string VowelDel(string str)
+        {
+            string vowels = "AaEeIiOoUuYy";
+            foreach (char e in vowels)
+            {
+                str = str.Replace(e, '\0');
+            }
+            return str;
+        }
+
+        public static string FiveClear(this string str)
+        {
+            str = str.Remove(0, 5);
+            return str;
+        }
+
+        public static string Sum(this string first, string second)
+        {
+
+            first = String.Concat(first, second);
+            return first;
+        }
+        public static int LengthDiff(this string first, string second)
+        {
+            int diff = Math.Abs(first.Length - second.Length);
+            return diff;
+        }
+        public static int CountOfSymbols(this string str)
+        {
+            return str.Length;
+        }
+
+        /*public static void Show(this Array arr)
+        {
+            foreach (int e in arr._array)
+            {
+                Console.WriteLine(e);
+            }
+        }*/
+    }
+}
